19b41d334f73faa4930691f42bae5ec0e0c64c77
artem <KudinovArtem@outlook.com>
2022-10-26 23:01:41 +0600

    Task01 добавил ввод с клавиатуры максимальное значение до которого посчитать сумму

--- src/Task01.java
+++ src/Task01.java
@@ -1,19 +1,23 @@
+import java.util.Scanner;
 public class Task01 {
     /*
 Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9.
 Сумма этих чисел равна 23.
 
-    Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+    Найдите сумму всех чисел меньше (1000 - или вводит пользователь значение), кратных 3 или 5.
  */
     public static void main(String[] args) {
         int summa = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Найти сумму всех чисел меньше чем: ");
+        int max = in.nextInt();
 
-        for (int n = 1; n < 1000; n++ ) {
+        for (int n = 1; n < max; n++ ) {
             if (n % 3 == 0 || n % 5 ==0) {
                 summa +=n;
             }
 
         }
-        System.out.println("Сумма всех чисел меньше 1000, кратных 3 или 5: " + summa);
+        System.out.println("Сумма всех чисел меньше " + max + ", кратных 3 или 5: " + summa);
     }
 }
